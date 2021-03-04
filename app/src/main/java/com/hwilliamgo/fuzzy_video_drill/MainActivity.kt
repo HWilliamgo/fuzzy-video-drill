3bdfc202396e50fe358bff20c60fb5b9aa0f09b8
HWilliamgo <williamintoandroid@outlook.com>
2021-03-04 20:21:40 +0800

    first commit

---
+++ app/src/main/java/com/hwilliamgo/fuzzy_video_drill/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hwilliamgo.fuzzy_video_drill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
