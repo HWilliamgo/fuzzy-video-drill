6aa66021dbfdfd8068c51bb56a990c7e5eaa42f9
HWilliamgo <williamintoandroid@outlook.com>
2021-03-11 00:32:01 +0800

    init video call

---
+++ app/src/main/java/com/hwilliamgo/fuzzy_video_drill/videocall/VideoCallActivity.kt
@@ -0,0 +1,12 @@
+package com.hwilliamgo.fuzzy_video_drill.videocall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hwilliamgo.fuzzy_video_drill.R
+
+class VideoCallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_call)
+    }
+}
